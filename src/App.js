d416ce1aa3ddad276815a0cff4d8396f8a4e9aef
xtaaaal <crystal.hon@arch-partnership.com>
2019-08-28 18:56:35 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './common.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main-scene">
+        <section className="flight">
+          <h1>航班信息</h1>
+          <div className="flight-info">
+            
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
