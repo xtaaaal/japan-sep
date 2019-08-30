244511dc1c79ca5412889a9f0f3b2e5d336a3c3e
xtaaaal <xtaaaal@gmail.com>
2019-08-30 22:42:07 +0800

    Updates

---
+++ precache-manifest.4582cc42a6cd98dcd13d16176dbf9c9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31848bd800b2d388d8a7d68ec295528",
+    "url": "/japan-sep/index.html"
+  },
+  {
+    "revision": "b410efd8e85823969216",
+    "url": "/japan-sep/static/css/main.67b4ac30.chunk.css"
+  },
+  {
+    "revision": "65784b20d6f978076476",
+    "url": "/japan-sep/static/js/2.81f610c3.chunk.js"
+  },
+  {
+    "revision": "b410efd8e85823969216",
+    "url": "/japan-sep/static/js/main.2ef77816.chunk.js"
+  },
+  {
+    "revision": "9bf114eb64aae3218bdf",
+    "url": "/japan-sep/static/js/runtime~main.f51a8485.js"
+  },
+  {
+    "revision": "c2cf888a58ee4bbea44f139b5b6e9f41",
+    "url": "/japan-sep/static/media/fuji-mountain.c2cf888a.svg"
+  },
+  {
+    "revision": "e5adbca1e56998726be9d7bb27a2ff8d",
+    "url": "/japan-sep/static/media/logo.e5adbca1.svg"
+  },
+  {
+    "revision": "a01b00173f29c6f050d8e17e7366b5fc",
+    "url": "/japan-sep/static/media/temple.a01b0017.svg"
+  },
+  {
+    "revision": "fc1ad3024d94b4f9b8dae428b4f09dc3",
+    "url": "/japan-sep/static/media/torii.fc1ad302.svg"
+  }
+]);
